fb90d3ed5860b492c737d4ff68c9b98ad5098a6b
sadylodr <yagor.rytvinski@stud.esdc.lt>
2025-04-27 17:08:08 +0300

    prompt window design

--- RichardAI/RichardAI/Views/MainWindow.axaml.cs
+++ RichardAI/RichardAI/Views/MainWindow.axaml.cs
@@ -1,11 +1,44 @@
+using System;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
 
-namespace RichardAI.Views;
-
-public partial class MainWindow : Window
+namespace RichardAI
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void MinimizeWindow(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        
+        private void WindowDragMove(object? sender, PointerPressedEventArgs e)
+        {
+            if (sender is Control control && control.GetVisualRoot() is Window window)
+            {
+                window.BeginMoveDrag(e);
+            }
+        }
+
+        private void SubmitButton_Click(object? sender, RoutedEventArgs e)
+        {
+            // Обработка нажатия кнопки
+            if (!string.IsNullOrWhiteSpace(InputTextBox.Text))
+            {
+                Console.WriteLine("Input: " + InputTextBox.Text);
+            }
+        }
     }
 }

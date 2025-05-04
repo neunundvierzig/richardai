3832d9bc2c2b6c7190fe955c692737bc735f9a46
Anatoli Liaucuk <anatolijlevcuk942@gmail.com>
2025-05-04 20:18:55 +0300

    feat: made source directory structure, added some winapi shit

--- RichardAI/RichardAI/Services/IKeyboardService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace RichardAI.Services;
-
-public interface IKeyboardService : IDisposable
-{
-    event Action HotKeyPressed;
-    void RegisterHotKey();
-}
---
+++ RichardAI/RichardAI/Source/UI/WinAPI/KeybordHook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RichardAI.Source.UI.WinAPI
+{
+    internal class KeybordHook : IHookable, IDisposable
+    {
+        // TODO: implement tha shi
+        public void Hook()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unhook()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

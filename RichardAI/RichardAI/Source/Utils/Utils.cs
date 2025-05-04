97fd2cfbf5db6bb495b963d74d2a93b96642171d
Anatoli Liaucuk <anatolijlevcuk942@gmail.com>
2025-05-04 20:26:45 +0300

    feat: made source directory structure, added some winapi shit

---
+++ RichardAI/RichardAI/Source/Utils/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RichardAI.Source.Utils
+{
+    internal class Utils
+    {
+        // dummy file to make directories visible
+    }
+}
